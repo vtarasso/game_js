8b9eb98c69aad8ebd9b08d01b3578aa7619b6938
Vitaliy Tarassov <106169405+vtarasso@users.noreply.github.com>
2024-01-16 16:01:39 +0600

    feat: add event for check input value

--- script.js
+++ script.js
@@ -1,98 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   const startButton = document.getElementById('start');
-//   const gameField = document.getElementById('game');
-//   const timeHeader = document.getElementById('time-header');
-//   const timeSpan = document.getElementById('time');
-//   const resultHeader = document.getElementById('result-header');
-//   const resultSpan = document.getElementById('result');
-//   const gameTimeInput = document.getElementById('game-time');
-
-//   let gameActive = false;
-//   let time = parseFloat(gameTimeInput.value);
-//   let timerId;
-//   let clickCount = 0;
-
-//   startButton.addEventListener('click', startGame);
-
-//   function startGame() {
-//     gameActive = true;
-//     startButton.style.display = 'none';
-//     gameField.style.backgroundColor = '#fff';
-
-//     // Убираем класс hide перед обновлением времени
-//     timeHeader.classList.remove('hide');
-//     resultHeader.classList.add('hide');
-
-//     time = parseFloat(gameTimeInput.value);
-//     timeSpan.textContent = time.toFixed(1);
-//     clickCount = 0;
-
-//     updateGameField();
-
-//     timerId = setInterval(updateTime, 100);
-//     setTimeout(endGame, time * 1000);
-//   }
-
-//   function updateGameField() {
-//     const cube = document.createElement('div');
-//     cube.className = 'cube';
-//     const size = Math.random() * 50 + 20;
-//     cube.style.width = `${size}px`;
-//     cube.style.height = `${size}px`;
-//     cube.style.backgroundColor = getRandomColor();
-//     cube.style.position = 'absolute';
-//     cube.style.top = `${Math.random() * (gameField.clientHeight - size)}px`;
-//     cube.style.left = `${Math.random() * (gameField.clientWidth - size)}px`;
-
-//     cube.addEventListener('click', function (event) {
-//       event.stopPropagation();
-//       gameField.removeChild(cube);
-//       clickCount++;
-//       updateGameField();
-//     });
-
-//     cube.addEventListener('mouseover', function () {
-//       cube.style.cursor = 'pointer';
-//     });
-
-//     gameField.appendChild(cube);
-//   }
-
-//   function updateTime() {
-//     if (gameActive) {
-//       time = Math.max(0, time - 0.1);
-//       timeSpan.textContent = time.toFixed(1);
-
-//       if (time <= 0) {
-//         endGame();
-//       }
-//     }
-//   }
-
-//   function endGame() {
-//     gameActive = false;
-//     clearInterval(timerId);
-//     timeHeader.classList.add('hide');
-//     resultHeader.classList.remove('hide');
-//     resultSpan.textContent = clickCount;
-//     startButton.style.display = 'block';
-//     gameField.style.backgroundColor = '#ccc';
-
-//     while (gameField.firstChild) {
-//       gameField.removeChild(gameField.firstChild);
-//     }
-//   }
-
-//   function getRandomColor() {
-//     const letters = '0123456789ABCDEF';
-//     let color = '#';
-//     for (let i = 0; i < 6; i++) {
-//       color += letters[Math.floor(Math.random() * 16)];
-//     }
-//     return color;
-//   }
-// });
-
 // Ждем, пока весь HTML-документ будет полностью загружен и готов к манипуляциям с DOM
 document.addEventListener('DOMContentLoaded', function () {
   // Получаем необходимые элементы DOM
@@ -103,6 +8,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const resultHeader = document.getElementById('result-header');
   const resultSpan = document.getElementById('result');
   const gameTimeInput = document.getElementById('game-time');
+
+  // Массив с цветами
   const colors = ['#fe019a', '#fe59c2', '#04d9ff', '#5555ff', '#bc13fe', '#f80000', '#ff073a', '#ffa420', '#ffa343', '#ffea19', '#fefe22', '#c8ff00', '#ccff00', '#7df9ff', '#39ff14'];
 
   // Объявляем переменные для управления состоянием игры и временем
@@ -131,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
     resultHeader.classList.add('hide');
 
     // Получаем время игры из поля ввода и обновляем соответствующий элемент DOM
-    time = parseFloat(gameTimeInput.value);
+    // time = parseFloat(gameTimeInput.value);
     timeSpan.textContent = time.toFixed(1);
     // Сбрасываем счетчик кликов
     clickCount = 0;
@@ -215,13 +122,15 @@ document.addEventListener('DOMContentLoaded', function () {
     while (gameField.firstChild) {
       gameField.removeChild(gameField.firstChild);
     }
-
-    // if (!gameActive) {
-    //   resultHeader.classList.add('hide');
-    //   timeHeader.classList.remove('hide');
-    //   time = parseFloat(gameTimeInput.value);
-    //   timeSpan.textContent = time.toFixed(1);
-    // } 
+  
+    // Взаимодействие с инпутом, чтобы убирать строку результата и получать время
+    gameTimeInput.addEventListener('input', function() {
+      resultHeader.classList.add('hide');
+      timeHeader.classList.remove('hide');
+      // time = parseFloat(gameTimeInput.value);
+      timeSpan.textContent = time.toFixed(1);
+      // console.log('Значение инпута изменилось:', gameTimeInput.value);
+    });
   }
 
   // Функция генерирует случайный цвет
@@ -230,5 +139,3 @@ document.addEventListener('DOMContentLoaded', function () {
     return colors[randomIndex];
   }
 });
-
-
